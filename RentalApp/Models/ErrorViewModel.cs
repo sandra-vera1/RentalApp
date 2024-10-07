c9f97af56baf413c2efaa4bcd65fedb85386a4c8
sandra-vera1 <sandra-vera1@hotmail.com>
2024-10-07 10:11:51 -0600

    Add project files.

---
+++ RentalApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RentalApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
