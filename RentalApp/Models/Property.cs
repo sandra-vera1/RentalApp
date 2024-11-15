6e1f1b87fc41d0dfd51212d9eb0bd296cf048906
KailanB <kailanbates@gmail.com>
2024-11-14 17:13:27 -0700

    Changed property class to reflect DB and other changes

--- RentalApp/Models/Property.cs
+++ RentalApp/Models/Property.cs
@@ -1,31 +1,80 @@
-﻿namespace RentalApp.Models
+﻿
+using System.Data.SqlClient;
+using System.Data;
+
+namespace RentalApp.Models
 {
     public class Property
     {
+        private readonly string _connectionString;
+
+
         public int PropertyId { get; private set; }
-        public Address PropertyAddress { get; private set; }//added public
+        public int AddressId { get; private set; }//added public
         public double SquareFootage { get; private set; }
-        public string[] FacilitiesProperty { get; private set; }
-        public string Term {  get; private set; }
+        public string Facilities { get; private set; }
+        public int TermId {  get; private set; }
         public string Type {  get; private set; }
         public bool Availability { get; private set; }
         public double Price { get; private set; }
 
+        public int OwnerId { get; private set; }
 
-        public Property(int propertyId, string neighborhood, int streetNum, string streetName, 
-            string city, int province, string country ,string postalCode, 
-            int suiteNum, double sqFt,string term, 
-            string type, bool availa, double price ) 
+
+
+
+        public Property(int addressId, double sqFt, string facilities, int termId, 
+            string type, bool availa, double price, int ownerId ) 
 
         { 
-            PropertyId = propertyId;
-            PropertyAddress = new Address( neighborhood, streetNum, streetName, city, province, country,postalCode, suiteNum);
+            // PropertyId = propertyId;
+            AddressId = addressId;
             SquareFootage = sqFt;
-            Term = term;
+            Facilities = facilities;
+            TermId = termId;
             Type = type;
             Availability = availa;
             Price = price;
+            OwnerId = ownerId;
 
+        }
+
+        public Property(string connectionString)
+        {
+            _connectionString = connectionString;
+
+        }
+
+
+        public bool CreateProperty(Property property)
+        {
+            int transaction = 0;
+            using(SqlConnection con = new SqlConnection(_connectionString))
+            {
+                using(SqlCommand cmd = new SqlCommand("CreateProperty", con))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.Add("@SqFt", SqlDbType.VarChar).Value = property.SquareFootage;
+                    cmd.Parameters.Add("@Facilities", SqlDbType.VarChar).Value = property.Facilities;
+                    cmd.Parameters.Add("@Type", SqlDbType.VarChar).Value = property.Type;
+
+                    cmd.Parameters.Add("@Price", SqlDbType.Money).Value = property.Price;
+
+                    cmd.Parameters.Add("@OwnerID", SqlDbType.Int).Value = property.OwnerId;
+                    cmd.Parameters.Add("@AddressID", SqlDbType.Int).Value = property.AddressId;
+                    cmd.Parameters.Add("@TermID", SqlDbType.Int).Value = property.TermId;
+
+                    cmd.Parameters.Add("@Availability", SqlDbType.Bit).Value = property.Availability;
+
+                   
+
+                    con.Open();
+                    transaction = cmd.ExecuteNonQuery();
+
+
+                }
+            }
+            return transaction == 1;
         }
 
 
