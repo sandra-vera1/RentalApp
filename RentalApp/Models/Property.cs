e7e1e0954c04355665507202ec34ec3acc80c0ab
KailanB <kailanbates@gmail.com>
2024-11-15 20:08:38 -0700

    added the get property list
    This method returns ALL details for a property including owner info, term info, address info etc.
    
    Also reorganized the properties within the Property class for clarity

--- RentalApp/Models/Property.cs
+++ RentalApp/Models/Property.cs
@@ -10,23 +10,38 @@ namespace RentalApp.Models
 
 
         public int PropertyId { get; private set; }
+
         public int AddressId { get; private set; }//added public
+
+        public Address Address { get; private set; } // to store address values ??
         public double SquareFootage { get; private set; }
         public string Facilities { get; private set; }
-        public int TermId {  get; private set; }
-        public string Type {  get; private set; }
+        public string Type { get; private set; }
         public bool Availability { get; private set; }
         public double Price { get; private set; }
 
+
+        // Term info
+        public int TermId { get; private set; }
+        public string Term {  get; private set; }
+
+
+        
+
+
+        // Owner information
         public int OwnerId { get; private set; }
+        public string OwnerName { get; private set; }
+        public string OwnerPhoneNum { get; private set; }
+        public string OwnerEmail {  get; private set; }
 
 
 
 
-        public Property(int addressId, double sqFt, string facilities, int termId, 
-            string type, bool availa, double price, int ownerId ) 
+        public Property(int addressId, double sqFt, string facilities, int termId,
+            string type, bool availa, double price, int ownerId)
 
-        { 
+        {
             // PropertyId = propertyId;
             AddressId = addressId;
             SquareFootage = sqFt;
@@ -49,9 +64,9 @@ namespace RentalApp.Models
         public bool CreateProperty(Property property)
         {
             int transaction = 0;
-            using(SqlConnection con = new SqlConnection(_connectionString))
+            using (SqlConnection con = new SqlConnection(_connectionString))
             {
-                using(SqlCommand cmd = new SqlCommand("CreateProperty", con))
+                using (SqlCommand cmd = new SqlCommand("CreateProperty", con))
                 {
                     cmd.CommandType = CommandType.StoredProcedure;
                     cmd.Parameters.Add("@SqFt", SqlDbType.VarChar).Value = property.SquareFootage;
@@ -66,7 +81,7 @@ namespace RentalApp.Models
 
                     cmd.Parameters.Add("@Availability", SqlDbType.Bit).Value = property.Availability;
 
-                   
+
 
                     con.Open();
                     transaction = cmd.ExecuteNonQuery();
@@ -78,16 +93,73 @@ namespace RentalApp.Models
         }
 
 
-        // I believe this was so that users could check the cost based on a duration at a property
-        // if the term is monthly and the renter would like to check a 6 month rental period then
-        // Price * Monthly * 6
-        // Unless we were intending a different use for this function
-        //public double CalculateTotalRentalCost(int duration)
-        //{
-        //    return Price * Term * duration;
-        //}
+        public List<Property> ListProperty()
+        {
+            List<Property> properties = new List<Property>();
+            // get a list of  all properties
+            using (SqlConnection con = new SqlConnection(_connectionString))
+            {
+                using (SqlCommand cmd = new SqlCommand("DisplayProperties", con))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    con.Open();
+                    SqlDataReader dr = cmd.ExecuteReader();
+                    while (dr.Read())
+                    {
+                        // Property data
+                        double SqFt = Convert.ToDouble(dr.GetString("SqFt"));
+                        string Facilities = dr.GetString("Facilities");
+                        string Type = dr.GetString("Type");
+                        double Price = Convert.ToDouble(dr.GetDecimal("Price"));
+                        int OwnerId = dr.GetInt32("OwnerID");
+                        int AddressId = dr.GetInt32("AddressID");
+                        bool Availability = dr.GetBoolean("Availability");
+                        Property prop = new Property(AddressId, SqFt, Facilities, TermId, Type, Availability, Price, OwnerId);
+                        
+                        // Owner data
+                        prop.OwnerName = dr.GetString("UserName");
+                        prop.OwnerPhoneNum = dr.GetString("PhoneNumber");
+                        prop.OwnerEmail = dr.GetString("Email");
+
+                        // Term data 
+                        prop.Term = dr.GetString("TermName");
+
+                        // Address data
+                        string Neigborhood = dr.GetString("Neighborhood");
+                        int StreetNumber = dr.GetInt32("StreetNumber");
+                        string StreetName = dr.GetString("StreetName");
+                        string City = dr.GetString("City");
+                        int ProvinceID = dr.GetInt32("ProvinceID");
+                        string ProvinceName = dr.GetString("ProvinceName");
+                        string Country = dr.GetString("Country");
+                        int SuiteNumber = dr.IsDBNull("SuiteNumber") ? 0 : dr.GetInt32("SuiteNumber");
+                        string PostalCode = dr.GetString("PostalCode");
+                        prop.Address = new Address(Neigborhood, StreetNumber, StreetName, City,
+                            ProvinceID, Country, PostalCode, SuiteNumber);
+                        prop.Address.ProvinceName = ProvinceName;
+
+                        properties.Add(prop);
+
+                    }
+                }
+
+                // trying to add an Address object to each property to easily display prop details
+
+            }
+
+
+            return properties;
+            // I believe this was so that users could check the cost based on a duration at a property
+            // if the term is monthly and the renter would like to check a 6 month rental period then
+            // Price * Monthly * 6
+            // Unless we were intending a different use for this function
+            //public double CalculateTotalRentalCost(int duration)
+            //{
+            //    return Price * Term * duration;
+            //}
 
 
 
+        }
     }
 }
