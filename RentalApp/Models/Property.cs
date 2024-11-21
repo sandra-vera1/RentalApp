22278bd59ed5bbbca19fa395357b7f97cb8f4db7
KailanB <kailanbates@gmail.com>
2024-11-20 17:24:50 -0700

    Cleaned up Property class Model to contain only data relevant to property

--- RentalApp/Models/Property.cs
+++ RentalApp/Models/Property.cs
@@ -6,38 +6,24 @@ namespace RentalApp.Models
 {
     public class Property
     {
-        private readonly string _connectionString;
-
+        // private readonly string _connectionString;
 
         public int PropertyId { get; private set; }
-
-        public int AddressId { get; private set; }//added public
-
-        public Address Address { get; private set; } // to store address values ??
-        public double SquareFootage { get; private set; }
-        public string Facilities { get; private set; }
-        public string Type { get; private set; }
-        public bool Availability { get; private set; }
-        public double Price { get; private set; }
-
-
-        // Term info
-        public int TermId { get; private set; }
-        public string Term {  get; private set; }
-
-
-        
-
-
-        // Owner information
+        // these all need to be public in order to use asp-for
+        public int AddressId { get; set; }//added public
+        public Address Address { get; set; } // to store address values ??
+        public double SquareFootage { get; set; }
+        public string Facilities { get; set; }
+        public string Type { get; set; }
+        public bool Availability { get; set; }
+        public double Price { get; set; }
+        public int TermId { get; set; }
         public int OwnerId { get; private set; }
-        public string OwnerName { get; private set; }
-        public string OwnerPhoneNum { get; private set; }
-        public string OwnerEmail {  get; private set; }
 
-
-
-
+        // empty constructor is require to use asp-for
+        public Property()
+        {
+        }
         public Property(int addressId, double sqFt, string facilities, int termId,
             string type, bool availa, double price, int ownerId)
 
@@ -51,115 +37,114 @@ namespace RentalApp.Models
             Availability = availa;
             Price = price;
             OwnerId = ownerId;
-
         }
 
-        public Property(string connectionString)
-        {
-            _connectionString = connectionString;
+        //public Property(string connectionString)
+        //{
+        //    _connectionString = connectionString;
 
-        }
-
-
-        public bool CreateProperty(Property property)
-        {
-            int transaction = 0;
-            using (SqlConnection con = new SqlConnection(_connectionString))
-            {
-                using (SqlCommand cmd = new SqlCommand("CreateProperty", con))
-                {
-                    cmd.CommandType = CommandType.StoredProcedure;
-                    cmd.Parameters.Add("@SqFt", SqlDbType.VarChar).Value = property.SquareFootage;
-                    cmd.Parameters.Add("@Facilities", SqlDbType.VarChar).Value = property.Facilities;
-                    cmd.Parameters.Add("@Type", SqlDbType.VarChar).Value = property.Type;
-
-                    cmd.Parameters.Add("@Price", SqlDbType.Money).Value = property.Price;
-
-                    cmd.Parameters.Add("@OwnerID", SqlDbType.Int).Value = property.OwnerId;
-                    cmd.Parameters.Add("@AddressID", SqlDbType.Int).Value = property.AddressId;
-                    cmd.Parameters.Add("@TermID", SqlDbType.Int).Value = property.TermId;
-
-                    cmd.Parameters.Add("@Availability", SqlDbType.Bit).Value = property.Availability;
+        //}
 
 
+        //public bool CreateProperty(Property property)
+        //{
+        //    int transaction = 0;
+        //    using (SqlConnection con = new SqlConnection(_connectionString))
+        //    {
+        //        using (SqlCommand cmd = new SqlCommand("CreateProperty", con))
+        //        {
+        //            cmd.CommandType = CommandType.StoredProcedure;
+        //            cmd.Parameters.Add("@SqFt", SqlDbType.VarChar).Value = property.SquareFootage;
+        //            cmd.Parameters.Add("@Facilities", SqlDbType.VarChar).Value = property.Facilities;
+        //            cmd.Parameters.Add("@Type", SqlDbType.VarChar).Value = property.Type;
 
-                    con.Open();
-                    transaction = cmd.ExecuteNonQuery();
+        //            cmd.Parameters.Add("@Price", SqlDbType.Money).Value = property.Price;
+
+        //            cmd.Parameters.Add("@OwnerID", SqlDbType.Int).Value = property.OwnerId;
+        //            cmd.Parameters.Add("@AddressID", SqlDbType.Int).Value = property.AddressId;
+        //            cmd.Parameters.Add("@TermID", SqlDbType.Int).Value = property.TermId;
+
+        //            cmd.Parameters.Add("@Availability", SqlDbType.Bit).Value = property.Availability;
 
 
-                }
-            }
-            return transaction == 1;
-        }
+
+        //            con.Open();
+        //            transaction = cmd.ExecuteNonQuery();
 
 
-        public List<Property> ListProperty()
-        {
-            List<Property> properties = new List<Property>();
-            // get a list of  all properties
-            using (SqlConnection con = new SqlConnection(_connectionString))
-            {
-                using (SqlCommand cmd = new SqlCommand("DisplayProperties", con))
-                {
-                    cmd.CommandType = CommandType.StoredProcedure;
-                    con.Open();
-                    SqlDataReader dr = cmd.ExecuteReader();
-                    while (dr.Read())
-                    {
-                        // Property data
-                        double SqFt = Convert.ToDouble(dr.GetString("SqFt"));
-                        string Facilities = dr.GetString("Facilities");
-                        string Type = dr.GetString("Type");
-                        double Price = Convert.ToDouble(dr.GetDecimal("Price"));
-                        int OwnerId = dr.GetInt32("OwnerID");
-                        int AddressId = dr.GetInt32("AddressID");
-                        bool Availability = dr.GetBoolean("Availability");
-                        Property prop = new Property(AddressId, SqFt, Facilities, TermId, Type, Availability, Price, OwnerId);
+        //        }
+        //    }
+        //    return transaction == 1;
+        //}
+
+
+        //public List<Property> ListProperty()
+        //{
+        //    List<Property> properties = new List<Property>();
+        //    // get a list of  all properties
+        //    using (SqlConnection con = new SqlConnection(_connectionString))
+        //    {
+        //        using (SqlCommand cmd = new SqlCommand("DisplayProperties", con))
+        //        {
+        //            cmd.CommandType = CommandType.StoredProcedure;
+        //            con.Open();
+        //            SqlDataReader dr = cmd.ExecuteReader();
+        //            while (dr.Read())
+        //            {
+        //                // Property data
+        //                double SqFt = Convert.ToDouble(dr.GetString("SqFt"));
+        //                string Facilities = dr.GetString("Facilities");
+        //                string Type = dr.GetString("Type");
+        //                double Price = Convert.ToDouble(dr.GetDecimal("Price"));
+        //                int OwnerId = dr.GetInt32("OwnerID");
+        //                int AddressId = dr.GetInt32("AddressID");
+        //                bool Availability = dr.GetBoolean("Availability");
+        //                Property prop = new Property(AddressId, SqFt, Facilities, TermId, Type, Availability, Price, OwnerId);
                         
-                        // Owner data
-                        prop.OwnerName = dr.GetString("UserName");
-                        prop.OwnerPhoneNum = dr.GetString("PhoneNumber");
-                        prop.OwnerEmail = dr.GetString("Email");
+        //                // Owner data
+        //                //prop.OwnerName = dr.GetString("UserName");
+        //                //prop.OwnerPhoneNum = dr.GetString("PhoneNumber");
+        //                //prop.OwnerEmail = dr.GetString("Email");
 
-                        // Term data 
-                        prop.Term = dr.GetString("TermName");
+        //                // Term data 
+        //                prop.Term = dr.GetString("TermName");
 
-                        // Address data
-                        string Neigborhood = dr.GetString("Neighborhood");
-                        int StreetNumber = dr.GetInt32("StreetNumber");
-                        string StreetName = dr.GetString("StreetName");
-                        string City = dr.GetString("City");
-                        int ProvinceID = dr.GetInt32("ProvinceID");
-                        string ProvinceName = dr.GetString("ProvinceName");
-                        string Country = dr.GetString("Country");
-                        int SuiteNumber = dr.IsDBNull("SuiteNumber") ? 0 : dr.GetInt32("SuiteNumber");
-                        string PostalCode = dr.GetString("PostalCode");
-                        prop.Address = new Address(Neigborhood, StreetNumber, StreetName, City,
-                            ProvinceID, Country, PostalCode, SuiteNumber);
-                        prop.Address.ProvinceName = ProvinceName;
+        //                // Address data
+        //                string Neigborhood = dr.GetString("Neighborhood");
+        //                int StreetNumber = dr.GetInt32("StreetNumber");
+        //                string StreetName = dr.GetString("StreetName");
+        //                string City = dr.GetString("City");
+        //                int ProvinceID = dr.GetInt32("ProvinceID");
+        //                string ProvinceName = dr.GetString("ProvinceName");
+        //                string Country = dr.GetString("Country");
+        //                int SuiteNumber = dr.IsDBNull("SuiteNumber") ? 0 : dr.GetInt32("SuiteNumber");
+        //                string PostalCode = dr.GetString("PostalCode");
+        //                prop.Address = new Address(Neigborhood, StreetNumber, StreetName, City,
+        //                    ProvinceID, Country, PostalCode, SuiteNumber);
+        //                prop.Address.ProvinceName = ProvinceName;
 
-                        properties.Add(prop);
+        //                properties.Add(prop);
 
-                    }
-                }
+        //            }
+        //        }
 
-                // trying to add an Address object to each property to easily display prop details
+        //        // trying to add an Address object to each property to easily display prop details
 
-            }
-
-
-            return properties;
-            // I believe this was so that users could check the cost based on a duration at a property
-            // if the term is monthly and the renter would like to check a 6 month rental period then
-            // Price * Monthly * 6
-            // Unless we were intending a different use for this function
-            //public double CalculateTotalRentalCost(int duration)
-            //{
-            //    return Price * Term * duration;
-            //}
+        //    }
 
 
+        //    return properties;
+        //    // I believe this was so that users could check the cost based on a duration at a property
+        //    // if the term is monthly and the renter would like to check a 6 month rental period then
+        //    // Price * Monthly * 6
+        //    // Unless we were intending a different use for this function
+        //    //public double CalculateTotalRentalCost(int duration)
+        //    //{
+        //    //    return Price * Term * duration;
+        //    //}
 
-        }
+
+
+        //}
     }
 }
