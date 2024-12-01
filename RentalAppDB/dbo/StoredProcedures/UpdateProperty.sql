4bf7d1cc8e550ab3d0b775be68b43b159d56400a
KailanB <kailanbates@gmail.com>
2024-12-01 02:03:59 -0700

    Created updateProperty SP

---
+++ RentalAppDB/dbo/StoredProcedures/UpdateProperty.sql
@@ -0,0 +1,28 @@
+
+
+CREATE OR ALTER PROCEDURE [dbo].[UpdateProperty]
+	@PropertyId INT,
+	@SqFt VARCHAR(30),
+	@Facilities VARCHAR(100),
+	@Type VARCHAR(30),
+	@Price MONEY,
+	-- @OwnerID INT, 
+	@AddressID INT,
+	@TermID INT,
+	@Availability BIT
+
+AS BEGIN
+	UPDATE [dbo].[Properties] 
+	SET [SqFt] = @SqFt, 
+	[Facilities] = @Facilities, 
+	[Type] = @Type, 
+	[Price] = @Price, 
+	-- [OwnerID] = @OwnerID, shouldn't need this.
+	[AddressID] = @AddressID, 
+	[TermID] = @TermID, 
+	[Availability] = @Availability
+
+	WHERE PropertyID = @PropertyId
+END
+
+
