c792e1afcfc30c6a0b5feae3426b711358d8fc4e
KailanB <kailanbates@gmail.com>
2024-12-06 03:18:45 -0700

    Added new createView for the updates to create property dropdown menu

---
+++ RentalApp/ViewModels/CreatePropertyViewModel.cs
@@ -0,0 +1,29 @@
+﻿using RentalApp.Models;
+#nullable disable
+
+
+namespace RentalApp.ViewModels
+{
+
+
+    public class CreatePropertyViewModel
+    {
+
+
+        public int PropertyId {  get; set; }
+        public Address Address { get; set; }//added public
+        public int AddressId { get; set; }
+        public List<Address> AddressList { get; set; } 
+        public double SquareFootage { get; set; }
+        public string Facilities { get; set; }
+        public string Type { get; set; }
+        public bool Availability { get; set; }
+        public double Price { get; set; }
+        public List<Term> TermList {  get; set; }
+        public string Term { get; set; }
+        public int TermId { get; set; }
+
+
+
+    }
+}
