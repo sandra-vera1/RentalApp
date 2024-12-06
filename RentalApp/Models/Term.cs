67fe8395cbbd4ed3575c41e78efa977021cff8d8
KailanB <kailanbates@gmail.com>
2024-12-06 03:16:08 -0700

    Added Term class

---
+++ RentalApp/Models/Term.cs
@@ -0,0 +1,16 @@
+﻿namespace RentalApp.Models
+{
+    public class Term
+    {
+
+        public int TermID { get; set; }
+        public string TermName { get; set; }
+
+        public Term(int termID, string termName)
+        {
+            TermID = termID;
+            TermName = termName;
+        }
+
+    }
+}
