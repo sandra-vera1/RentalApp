d380302586a079dcd0e60f04ee6631bb92faa753
KailanB <kailanbates@gmail.com>
2024-12-09 14:05:05 -0700

    styling for search

--- RentalApp/wwwroot/js/site.js
+++ RentalApp/wwwroot/js/site.js
@@ -2,3 +2,20 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+let searchFilterbutton = document.getElementById("AdvancedSearchFilterButton");
+let searchFilterDiv = document.getElementById("SearchFilterDiv");
+
+
+searchFilterbutton.addEventListener("click", DisplayAdvancedSearchFilters);
+
+function DisplayAdvancedSearchFilters()
+{
+    if (searchFilterDiv.style.display == "block") {
+        searchFilterDiv.style.display = "none";
+    }
+    else {
+        searchFilterDiv.style.display = "block";
+    }
+
+
+}
